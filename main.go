96323b2e9ea21b966e9fad7154cb50281d37dd74
anonexistent <90211791+anonexistent@users.noreply.github.com>
2024-12-05 18:52:52 +0300

    Update main.go

--- main.go
+++ main.go
@@ -138,8 +138,8 @@ func main() {
 	r.Get("/potatoes/list", getAllPotatoes)
 
 	// Start the server
-	log.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", r); err != nil {
+	log.Println("Starting server on :54870")
+	if err := http.ListenAndServe(":54870", r); err != nil {
 		log.Fatalf("failed to start server: %v", err)
 	}
 }
