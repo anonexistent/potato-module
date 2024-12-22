87f676729f1f83509f3704e27a7236007ccfe34f
anonexistent <>
2024-12-22 12:10:14 +0500

    paginate

--- services/potatoService.go
+++ services/potatoService.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"potato-module/contracts"
 	"potato-module/models"
+	"strconv"
 
 	"github.com/go-chi/chi"
 )
@@ -63,13 +64,59 @@ func (s *Services) GetPotatoByID(w http.ResponseWriter, r *http.Request) {
 
 // GetAllPotatoes handles fetching all potatoes
 func (s *Services) GetAllPotatoes(w http.ResponseWriter, r *http.Request) {
+	// Получаем параметры пагинации из запроса
+	page := r.URL.Query().Get("page")
+	pageSize := r.URL.Query().Get("pageSize")
+
+	// Устанавливаем значения по умолчанию, если параметры не указаны
+	if page == "" {
+		page = "1"
+	}
+	if pageSize == "" {
+		pageSize = "6"
+	}
+
+	// Преобразуем параметры в целые числа
+	pageInt, err := strconv.Atoi(page)
+	if err != nil || pageInt < 1 {
+		http.Error(w, "Invalid page number", http.StatusBadRequest)
+		return
+	}
+
+	pageSizeInt, err := strconv.Atoi(pageSize)
+	if err != nil || pageSizeInt < 1 {
+		http.Error(w, "Invalid page size", http.StatusBadRequest)
+		return
+	}
+
+	// Вычисляем смещение
+	offset := (pageInt - 1) * pageSizeInt
 
 	var potatoes []models.Potato
-	if err := s.DB.Preload("Types").Preload("Sizes").Find(&potatoes).Error; err != nil {
+	if err := s.DB.Preload("Types").Preload("Sizes").Limit(pageSizeInt).Offset(offset).Find(&potatoes).Error; err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	// Подсчитываем общее количество записей
+	var totalRecords int64
+	if err := s.DB.Model(&models.Potato{}).Count(&totalRecords).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Вычисляем общее количество страниц
+	totalPages := (totalRecords + int64(pageSizeInt) - 1) / int64(pageSizeInt)
+
+	// Создаем структуру для ответа
+	response := struct {
+		Potatoes   []models.Potato `json:"potatoes"`
+		TotalPages int64           `json:"totalPages"`
+	}{
+		Potatoes:   potatoes,
+		TotalPages: totalPages,
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(potatoes)
+	json.NewEncoder(w).Encode(response)
 }
